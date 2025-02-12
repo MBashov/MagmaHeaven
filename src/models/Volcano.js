f8b6a0d1d2d7c21283aae6148b7dd1650219f82c
MBashov <galacticos.mb@gmail.com>
2025-02-12 14:28:20 +0100

    Add validations to user and volcano model

--- src/models/Volcano.js
+++ src/models/Volcano.js
@@ -4,30 +4,38 @@ const volcanoSchema = new Schema({
     name: {
         type: String,
         required: [true, 'Name is required!'],
+        minLength: [2, 'Name should be at least 2 characters'],
     },
     location: {
         type: String,
         required: [true, 'Location is required!'],
+        minLength: [3, 'Location should be at least 3 characters'],
     },
     elevation: {
         type: Number,
         required: [true, 'Elevation is required!'],
+        min: [0, 'Elevation should be minimum 0'],
     },
     lastEruption: {
         type: Number,
         required: [true, 'Last eruption is required!'],
+        min: [0, 'Year of Last Eruption should be between 0 and 2025'],
+        max: [2025, 'Year of Last Eruption should be between 0 and 2025'],
     },
     image: {
         type: String,
         required: [true, 'Image is required!'],
+        match: [/^https?:\/\//, 'Volcano Image should start with http:// or https://'],
     },
     typeVolcano: {
         type: String,
         required: [true, 'Type volcano is required!'],
+        enum: ['Type should be select between ["Supervolcanoes", "Submarine", "Subglacial", "Mud", "Stratovolcanoes", "Shield"]']
     },
     description: {
         type: String,
         required: [true, 'Description is required!'],
+        minLength: [10, 'Description should be a minimum of 10 characters long'],
     },
     voteList: [{
         type: Types.ObjectId,
