8464e047c51911497754d0bf3fa2429ac22d0aa4
MBashov <galacticos.mb@gmail.com>
2025-02-12 10:49:08 +0100

    Add register functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,29 @@
 import { Router } from "express";
 
-
+import authService from "../services/authService.js";
+import { AUTH_COOKIE_NAME } from "../config.js";
+import { isAuth } from "../middlewares/authMiddleware.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
 
 
 const authController = Router();
 
+authController.get('/register', (req, res) => {
+    res.render('auth/register');
+});
 
+authController.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    try {
+        const token = await authService.register(userData);
+
+        res.cookie(AUTH_COOKIE_NAME, token, { httpOnly: true });
+
+        res.redirect('/');
+    } catch (err) {
+        res.render('auth/register', { error: getErrorMessage(err), userData });
+    }
+});
 
 export default authController;
