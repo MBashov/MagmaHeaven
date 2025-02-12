322e71604a1d41e8308f80cf89561558d3c67243
MBashov <galacticos.mb@gmail.com>
2025-02-12 10:50:57 +0100

    Add login functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,6 +8,24 @@ import { getErrorMessage } from "../utils/errorUtils.js";
 
 const authController = Router();
 
+authController.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await authService.login(email, password);
+
+        res.cookie(AUTH_COOKIE_NAME, token, { httpOnly: true });
+
+        res.redirect('/');
+    } catch (err) {
+        res.render('auth/login', { error: getErrorMessage(err), email });
+    }
+});
+
 authController.get('/register', (req, res) => {
     res.render('auth/register');
 });
@@ -25,5 +43,7 @@ authController.post('/register', async (req, res) => {
         res.render('auth/register', { error: getErrorMessage(err), userData });
     }
 });
+
+
 
 export default authController;
